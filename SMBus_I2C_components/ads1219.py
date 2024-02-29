bc61339ffe6ab3b1e6296c1c6ee9122bc9a0d6fa
Colin Boulé <124721545+ColinB27@users.noreply.github.com>
2024-02-28 21:23:46 -0500

    Update ads1219.py

--- SMBus_I2C_components/ads1219.py
+++ SMBus_I2C_components/ads1219.py
@@ -10,10 +10,11 @@ from time import sleep
 
 class Ads1219():
     def __init__(self,addr, i2c=2):
-        self.addr = addr
+        self.addr = (addr)
         self.i2c = SMBus(i2c)
         self.ain_reg=((0x63, 0x83, 0xA3, 0xC3))
-        self.ain_values = [0,0,0,0] 
+        self.ain_raw_data = [0,0,0,0] 
+        self.ain_values   = [0,0,0,0] 
     
     def _read_ain_byte_data_(self, ain):
         self.i2c.write_byte_data(self.addr,0x40,self.ain_reg[ain]) # configures the adc to read a AN
@@ -21,13 +22,14 @@ class Ads1219():
         sleep(0.5)
         
         data = self.i2c.read_i2c_block_data(self.addr,0x10,3) # reads the value from the read action of the adc
-        self.ain_values[ain] = (((data[0] << 8)+ data[1])<< 8)+data[2]
+        self.ain_raw_data[ain] = (((data[0] << 8)+ data[1])<< 8)+data[2]
+        return data
     
     def _interpret_byte_data_(self,ain):
-        if (self.ain_values[ain] & 0x800000) == 0x800000: 
+        if (self.ain_raw_data[ain] & 0x800000) == 0x800000: 
             volts = "Value is negative and out of range"
         else: 
-            self.ain_values[ain] = round(((self.ain_values[ain]*5)/2**23),4)
+            self.ain_values[ain] = round(((self.ain_raw_data[ain]*5)/2**23),4)
     
     def _read_ain_voltage_(self, ain):
         self._read_ain_byte_data_(ain)
